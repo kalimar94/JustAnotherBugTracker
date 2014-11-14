bf75e69db6fb94f4e22a674fc49050bd06ff6e05
kalimar <kalimar@abv.bg>
2014-11-14 14:44:28 +0200

    Adding two comment models for issue and for ticket

---
+++ BugTrackingSystem/BugTrackingSystem.Models/Models/IssueComment.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BugTrackingSystem.Models
+{
+    public class IssueComment
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string CommentText { get; set; }
+
+        public string UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; }
+
+        public int IssueId { get; set; }
+
+        [ForeignKey("IssueId")]
+        public virtual Issue Issue { get; set; }
+
+    }
+}
---
+++ BugTrackingSystem/BugTrackingSystem.Models/Models/TicketComment.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BugTrackingSystem.Models
+{
+    public class TicketComment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string AuthorName { get; set; }
+
+        [Required]
+        public string AuthorEmail { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string commentText { get; set; }
+
+        public int TicketId { get; set; }
+
+        [ForeignKey("TicketId")]
+        public virtual Ticket Ticket { get; set; }
+
+    }
+}
