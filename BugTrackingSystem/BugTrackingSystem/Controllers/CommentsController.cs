8586a57218f0fd26e4e8f8625ce8091c33933617
kalimar <kalimar@abv.bg>
2014-11-14 20:47:07 +0200

    Changes to comments and issues views

--- BugTrackingSystem/BugTrackingSystem/Controllers/CommentsController.cs
+++ BugTrackingSystem/BugTrackingSystem/Controllers/CommentsController.cs
@@ -1,50 +1,59 @@
 ﻿using BugTrackingSystem.Data;
 using BugTrackingSystem.Data.Repositories;
+using BugTrackingSystem.Data.Repository.Units;
 using BugTrackingSystem.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using Microsoft.AspNet.Identity;
+
 
 namespace BugTrackingSystem.Controllers
 {
     public class CommentsController : Controller
     {
-        Repository<IssueComment> comments;
+        CommentUserUnit unitOfWork;
 
         public CommentsController()
         {
-            comments = new Repository<IssueComment>(new ApplicationDbContext());
+            this.unitOfWork = new CommentUserUnit(new ApplicationDbContext());
         }
 
-        public ActionResult View(int issueId)
+        public ActionResult View(int id)
         {
-            var model = comments.Where(x => x.IssueId == issueId);
-            return View(model);
+            var model = unitOfWork.Comments.Where(x => x.IssueId == id);
+            ViewBag.IssueId = id;
+            return PartialView(model);
         }
 
-        public ActionResult Create(int commentId)
+        public ActionResult Create(int id)
         {
-            return View();
-        }
+            var userId = User.Identity.GetUserId();
+            var user = unitOfWork.Users.Single(x => x.Id == userId);
 
-        [HttpPost]
-        public ActionResult Create(int commentId, IssueComment newComment)
-        {
-            return RedirectToAction("View", new { id = commentId });
-        }
-
-        public ActionResult Edit(int commentId, FormCollection CommentData)
-        {
-            var comment = comments.GetByID(commentId);
-            return View(comment);
+            return PartialView(new IssueComment { User = user });
         }
 
         [HttpPost]
-        public ActionResult Edit(int commentId)
+        public ActionResult Create(int id, IssueComment newComment)
         {
-            return RedirectToAction("View", new { id = commentId });
+            return RedirectToAction("View", new { id = id });
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int commentId, int id, FormCollection CommentData)
+        {
+            var comment = unitOfWork.Comments.GetByID(commentId);
+            return RedirectToAction("View", new { id = id });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var comment = unitOfWork.Comments.GetByID(id);
+            return PartialView(comment);
         }
     }
 }
