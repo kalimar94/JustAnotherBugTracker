fb15041d3eba1f978a40aba7a0f6c9c4ed5c5052
kalimar <kalimar@abv.bg>
2014-11-14 21:40:44 +0200

    Adding unit for comments and users

---
+++ BugTrackingSystem/BugTrackingSystem.Data/Repository/Units/CommentUserUnit.cs
@@ -0,0 +1,21 @@
+﻿using BugTrackingSystem.Data.Repositories;
+using BugTrackingSystem.Models;
+
+namespace BugTrackingSystem.Data.Repository.Units
+{
+    public class CommentUserUnit
+    {
+        private ApplicationDbContext context;
+
+        public Repository<User> Users { get; set; }
+
+        public Repository<IssueComment> Comments { get; set; }
+
+        public CommentUserUnit(ApplicationDbContext context)
+        {
+            this.context = context;
+            this.Users = new Repository<User>(context);
+            this.Comments = new Repository<IssueComment>(context);
+        }
+    }
+}
