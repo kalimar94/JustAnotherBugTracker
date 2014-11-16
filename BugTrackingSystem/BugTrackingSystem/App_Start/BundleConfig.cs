16423819ab9b348a97b18695d59abf4c4383f95b
kalimar <kalimar@abv.bg>
2014-11-16 13:28:27 +0200

    Setting up Kendo, preparing base for issue administration controller

--- BugTrackingSystem/BugTrackingSystem/App_Start/BundleConfig.cs
+++ BugTrackingSystem/BugTrackingSystem/App_Start/BundleConfig.cs
@@ -8,8 +8,15 @@ namespace BugTrackingSystem
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.IgnoreList.Clear();
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery")
+            .Include("~/Scripts/Kendo/jquery.min.js"));
+               // .Include("~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/kendo")
+                .Include("~/Scripts/Kendo/kendo.all.min.js",
+                         "~/Scirpts/Kendo/kendo.aspnetmvc.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -27,9 +34,15 @@ namespace BugTrackingSystem
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
 
+
+            bundles.Add(new StyleBundle("~/Content/kendo").Include(
+                      "~/Content/Kendo/kendo.common-bootstrap.min.css",
+                      "~/Content/Kendo/kendo.silver.min.css"));
+
+
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
-            BundleTable.EnableOptimizations = true;
+            BundleTable.EnableOptimizations = false;
         }
     }
 }
