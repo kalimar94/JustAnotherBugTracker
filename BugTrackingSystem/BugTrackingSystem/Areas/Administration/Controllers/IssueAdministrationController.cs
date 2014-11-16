023cf6cf95a0e374ca0d1dcbd27e9f6f13076af6
kalimar <kalimar@abv.bg>
2014-11-16 13:34:54 +0200

    Adding missing issue administration controller

---
+++ BugTrackingSystem/BugTrackingSystem/Areas/Administration/Controllers/IssueAdministrationController.cs
@@ -0,0 +1,36 @@
+﻿using BugTrackingSystem.Data.Repositories;
+using BugTrackingSystem.Models;
+using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BugTrackingSystem.Areas.Administration.Controllers
+{
+    public class IssueAdministrationController : Controller
+    {
+        private Repository<Issue> issues;
+
+        public IssueAdministrationController(Repository<Issue> issueRepository)
+        {
+            this.issues = issueRepository;
+        }
+
+        // GET: Administration/IssueAdministration
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetData([DataSourceRequest]DataSourceRequest request)
+        {
+
+            var issues = this.issues.ToDataSourceResult(request, ModelState);
+            return Json(issues);
+        }
+    }
+}
